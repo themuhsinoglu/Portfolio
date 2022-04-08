0259f00f12dc57f6abb6229790f5657f22fd9ff2
Ahmet Muhsinoğlu <93585502+themuhsinoglu@users.noreply.github.com>
2022-04-08 03:25:21 +0300

    AboutComponent added.

---
+++ Portfolio/Portfolio.UI/ViewComponents/About/AboutList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLogicLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.UI.ViewComponents.About
+{
+    public class AboutList:ViewComponent
+    {
+        AboutManager aboutManager = new AboutManager(new EFAboutDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = aboutManager.TGetList();
+            return View(values);
+        }
+    }
+}
