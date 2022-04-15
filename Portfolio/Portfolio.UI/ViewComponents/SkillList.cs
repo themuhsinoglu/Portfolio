f038f4772e9519d6975036dfd97dd6dd4de54c77
Ahmet Muhsinoğlu <93585502+themuhsinoglu@users.noreply.github.com>
2022-04-15 23:39:47 +0300

    Skill added

---
+++ Portfolio/Portfolio.UI/ViewComponents/SkillList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLogicLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.UI.ViewComponents
+{
+    public class SkillList : ViewComponent
+    {
+        SkillManager skillManager = new SkillManager(new EFSkillDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = skillManager.TGetList();
+            return View(values);
+        }
+    }
+}
