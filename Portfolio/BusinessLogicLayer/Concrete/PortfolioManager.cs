bc967c01013766c8c41e4311660c7521902566c3
Ahmet Muhsinoğlu <93585502+themuhsinoglu@users.noreply.github.com>
2022-04-16 00:18:18 +0300

    Portfolio component added

---
+++ Portfolio/BusinessLogicLayer/Concrete/PortfolioManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLogicLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Concrete
+{
+    public class PortfolioManager :IPortfolioService
+    {
+        EFPortfolioDal _portfolioDal;
+
+        public PortfolioManager(EFPortfolioDal portfolioDal)
+        {
+            _portfolioDal = portfolioDal;
+        }
+
+        public void TAdd(Portfolio t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Portfolio t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Portfolio TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Portfolio> TGetList()
+        {
+            return _portfolioDal.GetList();
+        }
+
+        public void TUpdate(Portfolio t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Portfolio/Portfolio.UI/ViewComponents/Portfolio/PortfolioList.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLogicLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.UI.ViewComponents.Portfolio
+{
+    public class PortfolioList : ViewComponent
+    {
+        
+
+            PortfolioManager portfolioManager = new PortfolioManager(new EFPortfolioDal());
+            public IViewComponentResult Invoke()
+            {
+                var values = portfolioManager.TGetList();
+                return View(values);
+            }
+        
+    }
+}
